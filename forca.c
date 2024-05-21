186954ab5888d277c210cd7f1b8a8efa5b2c4bce
Marcus <marcus@marcus-HP-Pavilion-14-Notebook-PC>
2024-05-21 18:53:41 -0300

    Created function acertou and enforcou

--- forca.c
+++ forca.c
@@ -4,7 +4,7 @@
 
 char palavra_secreta[20];
 char chutes[26];
-int tentativas = 0;
+int chutes_dados = 0;
 
 void inicio(){
     printf("---------------\n");
@@ -17,14 +17,14 @@ void chute(){
     printf("Qual letra? ");
     scanf(" %c", &palpite);
        
-    chutes[tentativas] = palpite;
-    tentativas++;
+    chutes[chutes_dados] = palpite;
+    chutes_dados++;
 }
 
 int ja_chutou(char letra){
     int achou = 0;
            
-    for(int j=0;j<tentativas;j++){
+    for(int j=0;j<chutes_dados;j++){
        if(chutes[j] == letra){
           achou = 1;
           break;
@@ -49,9 +49,33 @@ void escolhe_palavra(){
     sprintf(palavra_secreta, "MELANCIA");
 }
 
+int acertou(){
+    for(int i=0; i<strlen(palavra_secreta); i++){
+        if(!ja_chutou(palavra_secreta[i])){
+           return 0;
+        }
+    }
+    return 1;
+}
+
+int enforcou(){
+    int erros = 0;
+    
+    for(int i=0; i<chutes_dados; i++){
+        int existe = 0;
+        
+        for(int j=0;j<strlen(palavra_secreta);j++){
+            if(chutes[i] == palavra_secreta[j]){
+                existe = 1;
+                break;
+            }
+        }
+        if(!existe) erros++;
+    }
+    return erros >= 5;
+}
+
 int main(){
-    int acertou = 0;
-    int enforcou = 0;
     
     escolhe_palavra();
     inicio();
@@ -60,5 +84,5 @@ int main(){
        desenha_forca();
        chute();
 
-    }while(!acertou && !enforcou);
+    }while(!acertou() && !enforcou());
 }
